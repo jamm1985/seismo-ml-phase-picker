b0ea95d74587bcd11bc904c636bb5cfc4f36c99c
syler <syler1983.9@gmail.com>
2020-03-16 15:33:30 +1000

    split main.py, add utils with slicing functions

--- scripts/main.py
+++ scripts/main.py
@@ -2,14 +2,17 @@ import os
 import obspy.io.nordic.core as nordic_reader
 from obspy.core import read
 
+import utils.picks_slicing as picks
+
+# Main function body
 if __name__ == "__main__":
     path = '/seismo/seisan/'
     readings_path = path + 'REA/'
     waveforms_path = path + 'WAV/'
-    db_name = 'IMGG_/2017/04/'  # TO-DO: Change to just IMGG_, update file wav search
-    full_readings_path = readings_path + db_name + '/'
+    db_name = 'IMGG_'
+    full_readings_path = readings_path + db_name
     full_waveforms_path = waveforms_path + db_name
-    save_dir = '/home/chernykh/WAV'
+    save_dir = '/home/chernykh/WAV3'
 
     output_level = 0  # 0 - minimal output, 5 - maximum output
 
@@ -40,66 +43,6 @@ if __name__ == "__main__":
         print('Reading S-files:\n')
 
     for file in nordic_file_names:
-        if output_level >= 5:
-            print('Reading file ' + file)
-
-        try:
-            events = nordic_reader.read_nordic(file, True)  # Events tuple: (event.Catalog, [waveforms file names])
-        except nordic_reader.NordicParsingError as error:
-            corrupted_files_total += 1
-            if output_level >= 2:
-                print('In ' + file + ': ' + str(error))
-            continue
-        except ValueError as error:
-            if output_level >= 2:
-                print('In ' + file + ': ' + str(error))
-            continue
-
-        events_in_file = 0
-        index = -1
-
-        for event in events[0].events:
-            index += 1
-            try:
-                if len(event.picks) > 0:  # Only for files with picks
-                    with_picks_total += 1
-
-                    if output_level >= 3:
-                        print('File: ' + file + ' Event #' + str(events_in_file) + ' Picks: ' + str(len(event.picks)))
-
-                    for pick in event.picks:
-                        if output_level >= 3:
-                            print('\t' + str(pick))
-
-                        if output_level >= 3:
-                            print('\t' + 'Slices:')
-
-                        # Read and slice waveform
-                        for name in events[1][index]:
-                            wav_path = full_waveforms_path + name
-                            wav_st = read(wav_path)
-                            for trace in wav_st:
-                                trace_slice = trace.slice(pick.time)
-
-                                if output_level >= 3:
-                                    print('\t\t' + str(trace_slice))
-
-                                trace_slice.write(save_dir + '/' + name, format="MSEED")
-                                if output_level >= 4:
-                                    print('\t\t' + 'slice saved in path: ' + save_dir + '/' + name)
-                else:
-                    no_picks_total += 1
-
-                events_in_file += 1
-                events_total += 1
-            except ValueError as error:
-                if output_level >= 2:
-                    print('In ' + file + ': ' + str(error))
-                continue
-
-    if output_level >= 1:
-        print('Corrupted files: ' + str(corrupted_files_total))
-
-    if output_level >= 2:
-        print('Events total: ' + str(events_total))
-        print('Events with picks: ' + str(with_picks_total))
+        slices = picks.slice_from_reading(file, full_waveforms_path, output_level)
+        if slices != -1:
+            picks.save_traces(slices, save_dir)
---
+++ utils/picks_slicing.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import obspy.io.nordic.core as nordic_reader
+from obspy.core import read
+
+
+# Read S-file on reading_path and slice relevant waveforms in waveforms_path
+# Parameters:   reading_path    - string    path to S-file
+#               waveforms_path  - string    path to folder with waveform files
+#               output_level    - int       0 - min output, 5 - max output, default - 0
+# Returns:      -1                                  -    corrupted file
+#               [(obspy.core.trace.Trace, string)]  -    list of slice tuples: (slice, name of waveform file)
+def slice_from_reading(reading_path, waveforms_path, output_level=0):
+    if output_level >= 5:
+        print('Reading file: ' + reading_path)
+
+    try:
+        events = nordic_reader.read_nordic(reading_path, True)  # Events tuple: (event.Catalog, [waveforms file names])
+    except nordic_reader.NordicParsingError as error:
+        if output_level >= 2:
+            print('In ' + reading_path + ': ' + str(error))
+        return -1
+    except ValueError as error:
+        if output_level >= 2:
+            print('In ' + reading_path + ': ' + str(error))
+        return -1
+
+    index = -1
+    slices = []
+    for event in events[0].events:
+        index += 1
+        try:
+            if len(event.picks) > 0:  # Only for files with picks
+                if output_level >= 3:
+                    print('File: ' + reading_path + ' Event #' + str(index) + ' Picks: ' + str(len(event.picks)))
+
+                for pick in event.picks:
+                    if output_level >= 3:
+                        print('\t' + str(pick))
+
+                    if output_level >= 3:
+                        print('\t' + 'Slices:')
+
+                    # Read and slice waveform
+                    for name in events[1][index]:
+                        # Get WAV path from REA path
+                        splits = reading_path.split('/')
+                        year = splits[len(splits) - 3]
+                        month = splits[len(splits) - 2]
+
+                        # If failed: get WAV path from filename
+                        if len(year) != 4 or len(month) != 2:
+                            splits = name.split('-')
+                            year = splits[0]
+                            month = splits[1]
+                        wav_path = waveforms_path + '/' + year + '/' + month + '/' + name
+                        wav_st = read(wav_path)
+                        for trace in wav_st:
+                            trace_slice = trace.slice(pick.time)
+                            if output_level >= 3:
+                                print('\t\t' + str(trace_slice))
+                            slice_name_pair = (trace_slice, name)
+                            slices.append(slice_name_pair)
+
+        except ValueError as error:
+            if output_level >= 2:
+                print('In ' + reading_path + ': ' + str(error))
+            continue
+
+    return slices
+
+
+# Save trace/name tuples list to a file
+# Parameters:   traces      - [(obspy.core.trace.Trace, string)]    list of slice tuples: (slice, name of waveform file)
+#               save_dir    - string                                save path
+#               file_format - string                                format of same wave file, default - miniSEED "MSEED"
+def save_traces(traces, save_dir, file_format="MSEED"):
+    for trace in traces:
+        trace[0].write(save_dir + '/' + trace[1], format=file_format)
+
+
+# Normalizes provided path to: /something/something/something
+# Parameters:   path    - string    path to normalize
+# Returns:      string          -   normalized path
+def normalize_path(path):
+    while path[len(path) - 1] == ' ' or path[len(path) - 1] == '/':
+        path = path[:len(path) - 1]
+    return path
