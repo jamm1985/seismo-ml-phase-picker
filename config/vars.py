033edc58af4b1a23a3a294dfb1b66ad0a56dbff9
syler <syler1983.9@gmail.com>
2020-03-17 14:33:53 +1000

    Add config file and parameters support

---
+++ config/vars.py
@@ -0,0 +1,20 @@
+path = '/seismo/seisan/'
+db_name = 'IMGG_'
+save_dir = '/home/chernykh/WAV'
+wav_path = 'WAV/'
+rea_path = 'REA/'
+
+readings_path = path + rea_path
+waveforms_path = path + wav_path
+full_readings_path = readings_path + db_name
+full_waveforms_path = waveforms_path + db_name
+
+output_level = 0  # 0 - minimal output, 5 - maximum output
+
+help_message = """Usage: seismo-phase-picker [options]
+Options: 
+-h, --help \t\t : print this help message and exit
+-s, --save \t arg : directory to save picks traces
+-r, --rea \t arg : s-files database main directory
+-w. --wav \t arg : waveforms database main directory"""
+
---
+++ scripts/seismo-phase-picker.py
@@ -0,0 +1,58 @@
+import os
+import obspy.io.nordic.core as nordic_reader
+from obspy.core import read
+import sys
+import getopt
+
+import utils.picks_slicing as picks
+import config.vars as config
+
+# Main function body
+if __name__ == "__main__":
+    # Parse script parameters
+    argv = sys.argv[1:]
+
+    try:
+        opts, args = getopt.getopt(argv, 'hs:r:w:', ["help", "save=", "rea=", "wav="])
+    except getopt.GetoptError:
+        print(str(getopt.GetoptError))
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(config.help_message)
+            sys.exit()
+        elif opt in ("-s", "--save"):
+            config.save_dir = arg
+        elif opt in ("-r", "--rea"):
+            config.full_readings_path = arg
+        elif opt in ("-w", "--wav"):
+            config.full_waveforms_path = arg
+
+    # Get all nordic files in REA
+    nordic_dir_data = os.walk(config.full_readings_path)
+    nordic_file_names = []
+
+    for x in nordic_dir_data:
+        for file in x[2]:
+            nordic_file_names.append(x[0] + '/' + file)
+
+    # Print all nordic file names
+    if config.output_level >= 5:
+        print('All nordic files:\n')
+        for x in nordic_file_names:
+            print(x)
+
+    # Get and process all files with picks
+    events_total = 0
+    no_picks_total = 0
+    with_picks_total = 0
+    corrupted_files_total = 0
+
+    if config.output_level >= 5:
+        print('Reading S-files:\n')
+
+    for file in nordic_file_names:
+        slices = picks.slice_from_reading(file, config.full_waveforms_path, config.output_level)
+        if slices != -1:
+            picks.save_traces(slices, config.save_dir)
