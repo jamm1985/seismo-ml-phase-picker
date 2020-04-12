833aa95e4c4c371e86d464ffdc8b0fb7a995bff2
Vladimir <syler1983.9@gmail.com>
2020-04-13 00:45:31 +1000

    Changed sample length to 4 seconds
    This change is based on a sample length from the Generalized Seismic Phase Detection with Deep Learning article

--- config/vars.py
+++ config/vars.py
@@ -11,8 +11,8 @@ full_waveforms_path = waveforms_path + db_name
 
 output_level = 5  # 0 - minimal output, 5 - maximum output
 
-slice_duration = 5  # in seconds
-slice_offset = 5  # in seconds
+slice_duration = 4  # in seconds
+slice_offset = 5  # in seconds (negatively shifts start of waveform slicing from 1 to slice_offset seconds)
 
 help_message = """Usage: seismo-phase-picker [options]
 Options: 
